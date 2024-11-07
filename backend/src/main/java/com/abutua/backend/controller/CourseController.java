a4a3145afd51d3836611388f96581fcaddf52159
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-11-07 17:20:38 -0300

    adds GET courses

--- backend/src/main/java/com/abutua/backend/controller/CourseController.java
+++ backend/src/main/java/com/abutua/backend/controller/CourseController.java
@@ -1,5 +1,28 @@
 package com.abutua.backend.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.backend.model.Course;
+
+@RestController
+@CrossOrigin
 public class CourseController {
     
+    List<Course> courses = Arrays.asList(
+        new Course(1, "Java"),
+        new Course(2, "Angular"),
+        new Course(3, "React")
+    );
+
+    @GetMapping("courses")
+    public ResponseEntity<List<Course>> getCourses(){
+        return ResponseEntity.ok(courses);
+    }
+
 }
