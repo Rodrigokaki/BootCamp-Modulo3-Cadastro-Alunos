a431795e6af2e44972a73ada626fd5f24bf6d6e1
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-11-07 17:29:35 -0300

    closes #2 - backend

--- backend/src/main/java/com/abutua/backend/controller/StudentController.java
+++ backend/src/main/java/com/abutua/backend/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.abutua.backend.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
@@ -7,8 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.backend.model.Student;
 
@@ -41,6 +45,20 @@ public class StudentController {
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
 
         return ResponseEntity.ok(student);
+    }
+
+    @PostMapping("students")
+    public ResponseEntity<Student> postStudent(@RequestBody Student student){
+        student.setId(students.size()+1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(student.getId())
+        .toUri();
+
+        return ResponseEntity.created(location).body(student);
     }
 
 }
